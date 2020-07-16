2b5b0b9dc75aeaa140e1f857f4ceeb3f1f256d74
dcarrascor <dfcarrasco@uc.cl>
2020-07-15 20:22:41 -0400

    Delete manejar_todo.js

--- manejar_todo.js
+++
@@ -1,6 +0,0 @@
-$('.check:button').click(function(){
-    var checked = !$(this).data('checked');
-    $('input:checkbox').prop('checked', checked);
-    $(this).val(checked ? 'Dermarcar todo' : 'Marcar todo' )
-    $(this).data('checked', checked);
-});
